b6f86fca63fcbb8b90e6704ff4cadeecd4e88b15
abe <abesphone@live.co.uk>
2018-03-28 15:35:11 +0100

    first commit

---
+++ SignalRChat/Chatub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRChat.Hub;
+
+namespace SignalRChat
+{
+    public class Chatub : Hub
+    {
+        public Task Sendmessage(string user, string message)
+        {
+            string timestamp = DateTime.Now.ToShortTimeString();
+            return Clients.All.SendAsync("ReceiveMessage", timestamp, user, message);
+        }        
+    }
+}
